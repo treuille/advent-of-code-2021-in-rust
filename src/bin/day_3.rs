226db6ba089ee23ba77dbdf405be33bf8bd201fd
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-13 14:23:30 -0800

    Getting ready to solve 3b really nicely

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -1,12 +1,10 @@
-use std::iter;
-
 fn main() {
-    println!("Puzzle 3a: {:?} 2003336", solve_puzzle());
-    // println!("Puzzle 2b: {:?}", solve_puzzle(advance_b));
+    let (lines, more_ones) = process_input();
+    println!("Puzzle 3a: {:?} 2003336", solve_puzzle_a(&more_ones));
+    println!("Puzzle 3b: {:?}", solve_puzzle_b(&lines, &more_ones));
 }
 
-fn solve_puzzle() -> usize {
-    let (_, more_ones) = process_input();
+fn solve_puzzle_a(more_ones: &[bool]) -> usize {
     let (mut gamma, mut epsilon) = (0, 0);
     for (i, more_ones) in more_ones.iter().rev().enumerate() {
         if *more_ones {
@@ -18,13 +16,22 @@ fn solve_puzzle() -> usize {
     gamma * epsilon
 }
 
+fn solve_puzzle_b(_lines: &[String], _more_ones: &[bool]) -> usize {
+    0
+}
+
 fn process_input() -> (Vec<String>, Vec<bool>) {
+    // List of all the lines in the input file.
     let lines: Vec<String> = include_str!("../../puzzle_inputs/day_3.txt")
         .lines()
         .map(String::from)
         .collect();
+    let n_columns = lines[0].len();
+
+    // How many ones there are in each column.
     let ones: Vec<usize> = lines.iter().fold(
-        iter::repeat(0).take(lines[0].len()).collect(),
+        vec![0; n_columns],
+        // iter::repeat(0).take().collect(),
         |ones: Vec<usize>, line: &String| {
             ones.iter()
                 .zip(line.chars())
@@ -36,6 +43,8 @@ fn process_input() -> (Vec<String>, Vec<bool>) {
                 .collect()
         },
     );
+
+    // Whether there are more ones in each column.
     let more_ones: Vec<bool> = ones.iter().map(|ones| ones * 2 >= lines.len()).collect();
     (lines, more_ones)
 }
