602a0d3efc6bbd98d0e717844d7893655a9c7f88
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-12 19:43:33 -0800

    Cleaned up day 1 solution

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -4,7 +4,7 @@ fn main() {
 }
 
 fn solve_puzzle(window_size: usize) -> usize {
-    include_str!("../input.txt")
+    include_str!("../../puzzle_inputs/day_1.txt")
         .lines()
         .filter_map(|x| x.parse().ok())
         .collect::<Vec<usize>>()
