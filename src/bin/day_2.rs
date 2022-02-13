7f4b7584e3c95c3e9872ad14d8b4b067e6b3f120
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-13 00:46:51 -0800

    Solved day 2

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -1,15 +1,39 @@
 fn main() {
-    println!("Puzzle 2a: {:?}", "hello");
-    // println!("Puzzle 1b: {:?}", solve_puzzle(4));
+    println!("Puzzle 2a: {:?}", solve_puzzle(advance_a));
+    println!("Puzzle 2b: {:?}", solve_puzzle(advance_b));
 }
 
-fn solve_puzzle(_window_size: usize) -> usize {
-    0
-    // include_str!("../../puzzle_inputs/day_1.txt")
-    //     .lines()
-    //     .filter_map(|x| x.parse().ok())
-    //     .collect::<Vec<usize>>()
-    //     .windows(window_size)
-    //     .filter(|pair| pair[0] < pair[1])
-    //     .count()
+type State = (isize, isize, isize);
+
+fn solve_puzzle<F>(advance: F) -> isize
+where
+    F: Fn(State, &str, isize) -> State,
+{
+    let (x, y, _) =
+        include_str!("../../puzzle_inputs/day_2.txt")
+            .lines()
+            .fold((0, 0, 0), |state, line| {
+                let (direction, distance) = line.split_once(" ").unwrap();
+                let distance: isize = distance.parse().unwrap();
+                advance(state, direction, distance)
+            });
+    x * y
+}
+
+fn advance_a((x, y, _): State, direction: &str, distance: isize) -> State {
+    match direction {
+        "forward" => (x + distance, y, 0),
+        "up" => (x, y - distance, 0),
+        "down" => (x, y + distance, 0),
+        _ => panic!("Invalid direction"),
+    }
+}
+
+fn advance_b((x, y, aim): State, direction: &str, distance: isize) -> State {
+    match direction {
+        "forward" => (x + distance, y + distance * aim, aim),
+        "up" => (x, y, aim - distance),
+        "down" => (x, y, aim + distance),
+        _ => panic!("Invalid direction"),
+    }
 }
