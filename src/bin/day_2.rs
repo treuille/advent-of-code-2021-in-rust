ef06d2c1d703ce62d766ce160bd7547a8fd88702
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-12 19:46:02 -0800

    Starting to work on day 2

---
+++ src/bin/day_2.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Puzzle 2a: {:?}", "hello");
+    // println!("Puzzle 1b: {:?}", solve_puzzle(4));
+}
+
+fn solve_puzzle(_window_size: usize) -> usize {
+    0
+    // include_str!("../../puzzle_inputs/day_1.txt")
+    //     .lines()
+    //     .filter_map(|x| x.parse().ok())
+    //     .collect::<Vec<usize>>()
+    //     .windows(window_size)
+    //     .filter(|pair| pair[0] < pair[1])
+    //     .count()
+}
