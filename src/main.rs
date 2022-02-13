4d7583d666343c54714473da353a21c4050fd6ca
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-12 19:38:10 -0800

    Solved puzzle 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!!");
+    println!("Puzzle 1a: {:?}", solve_puzzle(2));
+    println!("Puzzle 1b: {:?}", solve_puzzle(4));
+}
+
+fn solve_puzzle(window_size: usize) -> usize {
+    include_str!("../input.txt")
+        .lines()
+        .filter_map(|x| x.parse().ok())
+        .collect::<Vec<usize>>()
+        .windows(window_size)
+        .filter(|pair| pair[0] < pair[1])
+        .count()
 }
