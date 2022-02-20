ed2ff1913b98e9800b9961b3e61d3778f709034f
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-20 01:13:21 -0800

    Further cleaned up day_5

--- src/bin/day_5.rs
+++ src/bin/day_5.rs
@@ -1,20 +1,16 @@
 use ndarray::Array2;
 use regex::Regex;
-use std::cmp;
 use std::cmp::{Ord, Ordering};
 use std::iter;
 
 type DynIter<Item> = Box<dyn Iterator<Item = Item>>;
 
 fn main() {
-    println!("Puzzle 5a: {} (6397)", solve(rectilinear_lines));
-    println!("Puzzle 5b: {} (22335)", solve(all_lines));
+    println!("Puzzle 5a: {} (6397)", solve(true));
+    println!("Puzzle 5b: {} (22335)", solve(false));
 }
 
-fn solve<F>(line_iter: F) -> usize
-where
-    F: Fn(usize, usize, usize, usize) -> DynIter<(usize, usize)>,
-{
+fn solve(skip_diagonals: bool) -> usize {
     let mut terrain: Array2<u16> = Array2::default((1000, 1000));
     let re = Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
     let input = include_str!("../../puzzle_inputs/day_5.txt");
@@ -25,29 +21,21 @@ where
         let x2: usize = caps[3].parse().unwrap();
         let y2: usize = caps[4].parse().unwrap();
 
-        for idx in line_iter(x1, y1, x2, y2) {
+        if skip_diagonals && (x1 != x2) && (y1 != y2) {
+            continue;
+        }
+
+        let [x_range, y_range]: [DynIter<usize>; 2] =
+            [(x1, x2), (y1, y2)].map(|(a, b)| -> DynIter<usize> {
+                match a.cmp(&b) {
+                    Ordering::Less => Box::new(a..=b),
+                    Ordering::Equal => Box::new(iter::repeat(a)),
+                    Ordering::Greater => Box::new((b..=a).rev()),
+                }
+            });
+        for idx in x_range.zip(y_range) {
             terrain[idx] += 1;
         }
     }
     terrain.iter().filter(|&x| x > &1).count()
-}
-
-fn rectilinear_lines(x1: usize, y1: usize, x2: usize, y2: usize) -> DynIter<(usize, usize)> {
-    match (x1 == x2, y1 == y2) {
-        (true, _) => Box::new((cmp::min(y1, y2)..=cmp::max(y1, y2)).map(move |y| (x1, y))),
-        (_, true) => Box::new((cmp::min(x1, x2)..=cmp::max(x1, x2)).map(move |x| (x, y1))),
-        _ => Box::new(iter::empty()),
-    }
-}
-
-fn all_lines(x1: usize, y1: usize, x2: usize, y2: usize) -> DynIter<(usize, usize)> {
-    let [x_range, y_range]: [DynIter<usize>; 2] =
-        [(x1, x2), (y1, y2)].map(|(a, b)| -> DynIter<usize> {
-            match a.cmp(&b) {
-                Ordering::Less => Box::new(a..=b),
-                Ordering::Equal => Box::new(iter::repeat(a)),
-                Ordering::Greater => Box::new((b..=a).rev()),
-            }
-        });
-    Box::new(x_range.zip(y_range))
 }
