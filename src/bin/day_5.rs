4cc34f411d87b682f828614ca8aae9b3f9326e8c
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-19 23:15:06 -0800

    Startign to work on day 5

---
+++ src/bin/day_5.rs
@@ -0,0 +1,9 @@
+use ndarray::Array2;
+// use std::mem;
+
+fn main() {
+    let terrain: Array2<u16> = Array2::default((1000, 1000));
+    include_str!("../../puzzle_inputs/day_5.txt")
+
+    println!("{input}");
+}
