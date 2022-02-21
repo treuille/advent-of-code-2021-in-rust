c40f58300f8267bbd8b8eb82ff9aaf21ebc8a5a7
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-02-20 21:45:51 -0800

    Solved 6b

--- src/bin/day_6.rs
+++ src/bin/day_6.rs
@@ -1,31 +1,23 @@
-// use ndarray::Array2;
-// use regex::Regex;
-// use std::cmp::{Ord, Ordering};
-// use std::iter;
-
-// type DynIter<Item> = Box<dyn Iterator<Item = Item>>;
-
 fn main() {
-    let mut fish: Vec<usize> = include_str!("../../puzzle_inputs/day_6.txt")
+    // Parse the input
+    let mut generations = [0usize; 9];
+    include_str!("../../puzzle_inputs/day_6.txt")
         .trim()
         .split(',')
-        .map(|s| s.parse().unwrap())
-        .collect();
-    println!("{:?}", fish);
+        .for_each(|s| {
+            let fish_age: usize = s.parse().unwrap();
+            generations[fish_age] += 1;
+        });
 
-    for day in 1..=256 {
-        let n_fish = fish.len();
-        for i in 0..n_fish {
-            fish[i] = match fish[i] {
-                0 => {
-                    fish.push(8);
-                    6
-                }
-                n => n.checked_sub(1).unwrap(),
-            }
-        }
-        println!("After day {}: {}", day, fish.len());
+    // Run the simulations.
+    println!("Puzzle 6a: {} (377263)", sim(generations, 80));
+    println!("Puzzle 6b: {} (1695929023803)", sim(generations, 256));
+}
+
+/// Returns the number of latern fish after `days` days of simulation.
+fn sim(mut generations: [usize; 9], days: usize) -> usize {
+    for day in 0..days {
+        generations[(day + 7) % 9] += generations[day % 9];
     }
-    // println!("Puzzle 5a: {} (6397)", solve(true));
-    // println!("Puzzle 5b: {} (22335)", solve(false));
+    generations.into_iter().sum()
 }
