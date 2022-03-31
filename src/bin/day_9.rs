9cb693792f192ba9d2c97605be7310feccd3d427
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-03-31 03:07:21 -0700

    Correctly parsing the input.

--- src/bin/day_9.rs
+++ src/bin/day_9.rs
@@ -1,6 +1,19 @@
-use ndarray::Array2;
+use ndarray::{s, Array, Array2, Ix1};
 
 fn main() {
-    println!("Puzzle 9a: {} (123)", 123);
-    println!("Puzzle 9b: {} (456)", 456);
+    // Parse the input into an Array2<u8>
+    let lines: Vec<&str> = include_str!("../../puzzle_inputs/day_9.txt")
+        .lines()
+        .collect();
+    let rows = lines.len();
+
+    let flat_input: Array<u8, Ix1> = Array::from_iter(
+        lines
+            .iter()
+            .flat_map(|line| line.chars().map(|c| c.to_string().parse().unwrap())),
+    );
+    let cols = flat_input.len() / rows;
+    let grid: Array2<u8> = flat_input.into_shape((rows, cols)).unwrap();
+    println!("shape: {rows}x{cols}");
+    println!("last row: {:?}", grid.slice(s![-1, ..]));
 }
