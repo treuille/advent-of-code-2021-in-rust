42cc0f294d0aa52eefa628763d69af19e4a6e0a6
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-02 19:09:37 -0700

    Solved 10a

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -1,13 +1,28 @@
 fn main() {
-    for (i, line) in read_input().into_iter().enumerate() {
-        println!("{i}: \"{line}\"");
-    }
-    println!("10a: {} (123)", solve_10a());
-    println!("10b: {} (456)", solve_10b());
+    let lines: Vec<&'static str> = read_input();
+    println!("10a: {} (123)", solve_10a(&lines));
+    // println!("10b: {} (456)", solve_10b());
 }
 
-fn solve_10a() -> usize {
-    123
+fn solve_10a(lines: &[&'static str]) -> usize {
+    lines
+        .iter()
+        .filter_map(|&line| {
+            let mut stack: Vec<char> = Vec::new();
+            line.chars().find_map(|c| match c {
+                '(' | '[' | '{' | '<' => {
+                    stack.push(c);
+                    None
+                }
+                ')' if stack.pop() != Some('(') => Some(3),
+                ']' if stack.pop() != Some('[') => Some(57),
+                '}' if stack.pop() != Some('{') => Some(1197),
+                '>' if stack.pop() != Some('<') => Some(25137),
+                ')' | ']' | '}' | '>' => None,
+                c => panic!("Unpected character: {c}"),
+            })
+        })
+        .sum()
 }
 
 fn solve_10b() -> usize {
