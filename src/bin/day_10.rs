5ddf6a04b4f576eff2d05bca17242ab3d7af9eb0
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-03 18:53:38 -0700

    Cleaned up the semantics of the day_10.rs

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -1,29 +1,28 @@
 fn main() {
-    let lines: Vec<Result<char, String>> = read_input();
+    let lines: Vec<Result<String, char>> = read_input();
     println!("10a: {} (369105)", solve_10a(&lines));
     println!("10b: {} (3999363569)", solve_10b(&lines));
 }
 
-fn solve_10a(lines: &[Result<char, String>]) -> usize {
+fn solve_10a(lines: &[Result<String, char>]) -> usize {
     lines
         .iter()
         .filter_map(|line| match line {
-            Ok(')') => Some(3),
-            Ok(']') => Some(57),
-            Ok('}') => Some(1197),
-            Ok('>') => Some(25137),
-            Ok(c) => panic!("Unexpected char: {c}"),
+            Err(')') => Some(3),
+            Err(']') => Some(57),
+            Err('}') => Some(1197),
+            Err('>') => Some(25137),
+            Err(c) => panic!("Unexpected char: {c}"),
             _ => None,
         })
         .sum()
 }
 
-fn solve_10b(lines: &[Result<char, String>]) -> usize {
+fn solve_10b(lines: &[Result<String, char>]) -> usize {
     let mut completion_points: Vec<usize> = lines
         .iter()
         .filter_map(|line| match line {
-            Ok(_) => None,
-            Err(s) => Some(s.chars().rev().fold(0, |result, c| {
+            Ok(s) => Some(s.chars().rev().fold(0, |result, c| {
                 (match c {
                     '(' => 1,
                     '[' => 2,
@@ -32,6 +31,7 @@ fn solve_10b(lines: &[Result<char, String>]) -> usize {
                     c => panic!("Unexpected char: {c}"),
                 }) + 5 * result
             })),
+            Err(_) => None,
         })
         .collect();
     completion_points.sort_unstable();
@@ -39,26 +39,28 @@ fn solve_10b(lines: &[Result<char, String>]) -> usize {
 }
 
 /// Convert input file into a Vec whose elments are either
-/// - Ok(c) if c was the corrupting character (for puzzle 10a)
-/// - Err(s) if s is the in complement of the completion sequence (for puzzle 10b)
-fn read_input() -> Vec<Result<char, String>> {
+/// - Ok(s) if s is the in complement of the completion sequence (for puzzle 10b)
+/// - Err(c) if c was the corrupting character (for puzzle 10a)
+fn read_input() -> Vec<Result<String, char>> {
     include_str!("../../puzzle_inputs/day_10.txt")
         .lines()
         .map(|line| {
             let mut stack = Vec::new();
-            line.chars()
-                .find_map(|c| match (stack.last(), c) {
-                    (_, '(') | (_, '[') | (_, '{') | (_, '<') => {
-                        stack.push(c);
-                        None
-                    }
-                    (Some('('), ')') | (Some('['), ']') | (Some('{'), '}') | (Some('<'), '>') => {
-                        stack.pop();
-                        None
-                    }
-                    (_, c) => Some(c),
-                })
-                .ok_or_else(|| stack.iter().collect())
+            let error_char = line.chars().find_map(|c| match (stack.last(), c) {
+                (_, '(') | (_, '[') | (_, '{') | (_, '<') => {
+                    stack.push(c);
+                    None
+                }
+                (Some('('), ')') | (Some('['), ']') | (Some('{'), '}') | (Some('<'), '>') => {
+                    stack.pop();
+                    None
+                }
+                (_, c) => Some(c),
+            });
+            match error_char {
+                None => Ok(stack.iter().collect()),
+                Some(error_char) => Err(error_char),
+            }
         })
         .collect()
 }
