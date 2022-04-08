5e8e2516649f38b2dccc4535e8199c1b60ac7049
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-07 23:39:00 -0700

    Parsed the input array for the puzzle.

--- src/bin/day_11.rs
+++ src/bin/day_11.rs
@@ -1,4 +1,7 @@
+use ndarray::prelude::*;
+
 /// This is the puzzle input I got.
+#[allow(dead_code)]
 const PUZZLE_INPUT: &str = "
 6227618536
 2368158384
@@ -13,7 +16,8 @@ const PUZZLE_INPUT: &str = "
 ";
 
 /// This is the example input in the puzzle description.
-const EXAMPLE_INPUT: &str = "
+#[allow(dead_code)]
+const EXAMPLE_INPUT_1: &str = "
 5483143223
 2745854711
 5264556173
@@ -26,20 +30,48 @@ const EXAMPLE_INPUT: &str = "
 5283751526
 ";
 
+/// Anohter example input in the puzzle description.
+#[allow(dead_code)]
+const EXAMPLE_INPUT_2: &str = "
+11111
+19991
+19191
+19991
+11111
+";
+
 /// Toggle between the two inputs;
-const INPUT: &str = PUZZLE_INPUT;
+const INPUT: &str = EXAMPLE_INPUT_2;
 
 fn main() {
-    println!("INPUT:\n{}", INPUT);
+    // Parse the input into an array.
+    let input_lines: Vec<&str> = INPUT.trim().lines().collect();
+    let shape = (input_lines.len(), input_lines[0].len());
+    let x: Array2<u8> = input_lines
+        .iter()
+        .flat_map(|&line| line.split("").filter_map(|s| s.parse().ok()))
+        .collect::<Array1<u8>>()
+        .into_shape(shape)
+        .expect("Array shape mismatch");
+    println!("{x:?}");
 
-    // println!("11a: {} (123)", solve_11a());
-    // println!("11b: {} (456)", solve_11b());
+    println!("step:\n{}", step(x));
 }
 
+fn step(x: Array2<u8>) -> Array2<u8> {
+    let y = &x + 1;
+    println!("{y:?}");
+    let z = y.map(|&elt| elt > 9);
+    println!("{z:?}");
+    y
+}
+
+#[allow(dead_code)]
 fn solve_11a() -> usize {
     123
 }
 
+#[allow(dead_code)]
 fn solve_11b() -> usize {
     456
 }
