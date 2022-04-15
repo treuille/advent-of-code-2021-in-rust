bf10ab2cd9750ae77d00b4b5de59c33f03cdcacc
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-15 16:47:41 -0700

    Using &str instead of enum. 0.14s - 81loc

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+// use regex::Regex;
 use std::collections::{HashMap, HashSet};
 
 fn main() {
@@ -8,63 +8,42 @@ fn main() {
 }
 
 fn read_input(input: &str) -> Puzzle {
-    let re = Regex::new(r"([[:alpha:]]+)-([[:alpha:]]+)").unwrap();
     let mut puzzle: Puzzle = Puzzle::new();
     for line in input.trim().lines() {
-        let caps = re.captures(line).unwrap();
-        let a = Cave::from_str(caps.get(1).unwrap().as_str());
-        let b = Cave::from_str(caps.get(2).unwrap().as_str());
-        puzzle.entry(a.clone()).or_default().insert(b.clone());
-        puzzle.entry(b.clone()).or_default().insert(a.clone());
+        let (a, b) = line.split_once("-").unwrap();
+        puzzle.entry(a).or_default().insert(b);
+        puzzle.entry(b).or_default().insert(a);
     }
     puzzle
 }
 
 fn solve_12a(puzzle: &Puzzle) -> usize {
-    Explorer::new(puzzle, false).explore(Cave::Start)
+    Explorer::new(puzzle, false).explore("start")
 }
 
 fn solve_12b(puzzle: &Puzzle) -> usize {
-    Explorer::new(puzzle, true).explore(Cave::Start)
+    Explorer::new(puzzle, true).explore("start")
 }
 
-/// The types of caves in the puzzle.
-#[derive(PartialEq, Eq, Hash, Clone)]
-enum Cave {
-    Start,
-    End,
-    Small(String),
-    Big(String),
-}
+/// Each cave is denoted by a string slice identifier.
+type Cave<'a> = &'a str;
 
 /// The puzzle input is a network of interconnected Caves.
-type Puzzle = HashMap<Cave, HashSet<Cave>>;
+type Puzzle<'a> = HashMap<Cave<'a>, HashSet<Cave<'a>>>;
 
 /// Stores the state of our exploration.
 struct Explorer<'a> {
     /// The puzzle we're exploring.
-    puzzle: &'a Puzzle,
+    puzzle: &'a Puzzle<'a>,
 
     /// Which caves we're exploring.
-    visited: HashSet<Cave>,
+    visited: HashSet<Cave<'a>>,
 
     /// Whether we've visitd this small cave twice
-    visited_twice: Option<Cave>,
+    visited_twice: Option<Cave<'a>>,
 
     /// Whether we can visit a small cave twice.
     can_visit_twice: bool,
-}
-
-impl Cave {
-    fn from_str(cave: &str) -> Self {
-        let is_small = |cave: &str| cave.chars().all(|c| ('a'..='z').contains(&c));
-        match cave {
-            "start" => Self::Start,
-            "end" => Self::End,
-            cave if is_small(cave) => Self::Small(cave.to_string()),
-            cave => Self::Big(cave.to_string()),
-        }
-    }
 }
 
 impl<'a> Explorer<'a> {
@@ -78,23 +57,24 @@ impl<'a> Explorer<'a> {
     }
 
     fn explore(&mut self, cave: Cave) -> usize {
+        let is_small = |cave: &str| cave.chars().all(|c| ('a'..='z').contains(&c));
         let mut paths = 0;
-        for next_cave in self.puzzle.get(&cave.clone()).unwrap() {
-            match next_cave {
-                Cave::Start => (),
-                Cave::End => paths += 1,
-                Cave::Big(_) => paths += self.explore(next_cave.clone()),
-                Cave::Small(_) => {
+        for next_cave in self.puzzle.get(&cave).unwrap() {
+            match *next_cave {
+                "start" => (),
+                "end" => paths += 1,
+                next_cave if is_small(next_cave) => {
                     if !self.visited.contains(next_cave) {
-                        self.visited.insert(next_cave.clone());
-                        paths += self.explore(next_cave.clone());
+                        self.visited.insert(next_cave);
+                        paths += self.explore(next_cave);
                         self.visited.remove(next_cave);
                     } else if self.can_visit_twice && self.visited_twice.is_none() {
-                        self.visited_twice = Some(next_cave.clone());
-                        paths += self.explore(next_cave.clone());
+                        self.visited_twice = Some(next_cave);
+                        paths += self.explore(next_cave);
                         self.visited_twice = None;
                     }
                 }
+                next_cave => paths += self.explore(next_cave),
             }
         }
         paths
