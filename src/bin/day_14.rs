e096041135459d214c7bf418c091c985e8a46a54
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-16 00:01:47 -0700

    Starting to work on puzzle 13

--- src/bin/day_14.rs
+++ src/bin/day_14.rs
@@ -2,21 +2,21 @@ fn main() {
     for (i, line) in read_input().into_iter().enumerate() {
         println!("{i}: \"{line}\"");
     }
-    println!("13a: {} (123)", solve_13a());
-    println!("13b: {} (456)", solve_13b());
+    println!("10a: {} (123)", solve_10a());
+    println!("10b: {} (456)", solve_10b());
 }
 
-fn solve_13a() -> usize {
+fn solve_10a() -> usize {
     123
 }
 
-fn solve_13b() -> usize {
+fn solve_10b() -> usize {
     456
 }
 
 /// Read the input file and turn it into an Array2<u8>
 fn read_input() -> Vec<&'static str> {
-    include_str!("../../puzzle_inputs/day_13.txt")
+    include_str!("../../puzzle_inputs/day_10.txt")
         .lines()
         .collect()
 }
