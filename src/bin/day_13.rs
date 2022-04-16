d3a8682dcbc404f20a44102a608539ad6dc75115
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-16 02:47:01 -0700

    Solved 13b (81 loc)

--- src/bin/day_13.rs
+++ src/bin/day_13.rs
@@ -6,46 +6,26 @@ use std::str::FromStr;
 type Point = (usize, usize);
 type Points = HashSet<Point>;
 
-#[allow(dead_code)]
-const TEST_INPUT: &str = "
-6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
-
-fold along y=7
-fold along x=5
-";
-
 fn main() {
+    // Load the input points.
     let input = include_str!("../../puzzle_inputs/day_13.txt");
     let (points, folds) = input.split_once("\n\n").unwrap();
-    // let (points, folds) = TEST_INPUT.split_once("\n\n").unwrap();
-    let points: Points =
+    let mut points: Points =
         Points::from_iter(points.lines().filter_map(|line| parse_point(line).ok()));
-    println!("points: {points:?}");
-    print_pts(&points);
 
+    // Apply each fold in succession.
     let folds: Vec<Fold> = folds.lines().filter_map(|line| line.parse().ok()).collect();
-    println!("folds: {folds:?}");
+    for (i, fold) in folds.iter().enumerate() {
+        if i == 1 {
+            println!("13a: {} (592)", points.len());
+        }
+        points = fold.fold(points);
+    }
 
-    let points = folds[0].fold(points);
+    // Display the second answer.
+    println!("13b:");
     print_pts(&points);
-    println!("answer: {}", points.len());
+    println!("(JGAJEFKU)");
 }
 
 fn print_pts(points: &Points) {
@@ -55,29 +35,6 @@ fn print_pts(points: &Points) {
     for y in 0..=h {
         for x in 0..=w {
             print!("{}", display_char[&points.contains(&(x, y))]);
-        }
-        println!();
-    }
-}
-
-#[allow(dead_code)]
-fn print_pts_old(points: &Points) {
-    let width = points.iter().map(|(x, _)| x).max().unwrap();
-    let mut points: Vec<&Point> = points.iter().collect();
-    points.sort_unstable_by_key(|(x, y)| (y, x));
-    let mut points = points.into_iter().peekable();
-    for y in 0usize.. {
-        if points.peek().is_none() {
-            break;
-        }
-        for x in 0..=*width {
-            match points.peek() {
-                Some(&&pt) if pt == (x, y) => {
-                    points.next();
-                    print!("X");
-                }
-                _ => print!(","),
-            }
         }
         println!();
     }
@@ -99,8 +56,6 @@ impl Fold {
         Points::from_iter(points.iter().map(|&(x, y)| match *self {
             Fold::X(location) if x > location => (2 * location - x, y),
             Fold::Y(location) if y > location => (x, 2 * location - y),
-            Fold::X(location) if x == location => panic!("Point on fold"),
-            Fold::X(location) if y == location => panic!("Point on fold"),
             _ => (x, y),
         }))
     }
@@ -124,14 +79,3 @@ impl FromStr for Fold {
         }
     }
 }
-
-fn solve_13a() -> usize {
-    123
-}
-
-fn solve_13b() -> usize {
-    456
-}
-
-// /// Read the input file and turn it into an Array2<u8>
-// fn read_input() -> Vec<&'static str> {}
