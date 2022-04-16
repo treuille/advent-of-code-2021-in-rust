4f94b0f8e9b30143afd4f3cee513cfc053bdd35c
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-16 13:47:56 -0700

    Strating to work on day 14

--- src/bin/day_14.rs
+++ src/bin/day_14.rs
@@ -2,21 +2,21 @@ fn main() {
     for (i, line) in read_input().into_iter().enumerate() {
         println!("{i}: \"{line}\"");
     }
-    println!("10a: {} (123)", solve_10a());
-    println!("10b: {} (456)", solve_10b());
+    println!("14a: {} (123)", solve_14a());
+    println!("14b: {} (456)", solve_14b());
 }
 
-fn solve_10a() -> usize {
+fn solve_14a() -> usize {
     123
 }
 
-fn solve_10b() -> usize {
+fn solve_14b() -> usize {
     456
 }
 
 /// Read the input file and turn it into an Array2<u8>
 fn read_input() -> Vec<&'static str> {
-    include_str!("../../puzzle_inputs/day_10.txt")
+    include_str!("../../puzzle_inputs/day_14.txt")
         .lines()
         .collect()
 }
