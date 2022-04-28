cd996e25b8bb6db5b19ab025943064717d52e09c
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-28 12:15:00 -0700

    Implemented FromRegex which seems like a nicer pattern

--- src/bin/day_02.rs
+++ src/bin/day_02.rs
@@ -1,38 +1,39 @@
-use aoc::to_tuple::ToTuple;
-use regex::Regex;
+// use aoc::to_tuple::ToTuple;
+// use regex::Regex;
 
 fn main() {
-    println!("Puzzle 2a: {:?}", solve_puzzle(advance_a));
-    println!("Puzzle 2b: {:?}", solve_puzzle(advance_b));
+    todo!("Redo with the new crate.");
+    // println!("Puzzle 2a: {:?}", solve_puzzle(advance_a));
+    // println!("Puzzle 2b: {:?}", solve_puzzle(advance_b));
 }
 
-type State = (isize, isize, isize);
+// type State = (isize, isize, isize);
 
-fn solve_puzzle<F>(advance: F) -> isize
-where
-    F: Fn(State, &str, isize) -> State,
-{
-    let re = Regex::new(r"([a-z]+) (\d)").unwrap();
-    let (x, y, _) = re
-        .parse_lines(include_str!("../../puzzle_inputs/day_02.txt"))
-        .fold((0, 0, 0), |state, (dir, dist)| advance(state, dir, dist));
-    x * y
-}
+// fn solve_puzzle<F>(advance: F) -> isize
+// where
+//     F: Fn(State, &str, isize) -> State,
+// {
+//     let re = Regex::new(r"([a-z]+) (\d)").unwrap();
+//     let (x, y, _) = re
+//         .parse_lines(include_str!("../../puzzle_inputs/day_02.txt"))
+//         .fold((0, 0, 0), |state, (dir, dist)| advance(state, dir, dist));
+//     x * y
+// }
 
-fn advance_a((x, y, _): State, direction: &str, distance: isize) -> State {
-    match direction {
-        "forward" => (x + distance, y, 0),
-        "up" => (x, y - distance, 0),
-        "down" => (x, y + distance, 0),
-        _ => panic!("Invalid direction"),
-    }
-}
+// fn advance_a((x, y, _): State, direction: &str, distance: isize) -> State {
+//     match direction {
+//         "forward" => (x + distance, y, 0),
+//         "up" => (x, y - distance, 0),
+//         "down" => (x, y + distance, 0),
+//         _ => panic!("Invalid direction"),
+//     }
+// }
 
-fn advance_b((x, y, aim): State, direction: &str, distance: isize) -> State {
-    match direction {
-        "forward" => (x + distance, y + distance * aim, aim),
-        "up" => (x, y, aim - distance),
-        "down" => (x, y, aim + distance),
-        _ => panic!("Invalid direction"),
-    }
-}
+// fn advance_b((x, y, aim): State, direction: &str, distance: isize) -> State {
+//     match direction {
+//         "forward" => (x + distance, y + distance * aim, aim),
+//         "up" => (x, y, aim - distance),
+//         "down" => (x, y, aim + distance),
+//         _ => panic!("Invalid direction"),
+//     }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-pub mod to_tuple {
+pub mod parse_regex {
     use regex::Regex;
-    use std::marker::PhantomData;
-    use std::str::Lines;
 
+    pub fn from_regex<'a, T>(re: &Regex, s: &'a str) -> T
+        where T: FromRegex<'a> {
+            T::parse(re, s)
+        }
+
+    // TODO; Try removeing `pub`
     pub trait FromStr<'a> {
         fn from_str(s: &'a str) -> Self;
     }
@@ -31,48 +35,18 @@ pub mod to_tuple {
         }
     }
 
-    pub trait ToTuple<'a, Tuple>
-    where
-        Self: Sized,
-    {
-        fn to_tuple(&self, s: &'a str) -> Tuple;
-
-        fn parse_lines(&'a self, lines: &'a str) -> LinesToTuples<'a, Tuple, Self> {
-            LinesToTuples {
-                str_parser: self,
-                lines: lines.trim().lines(),
-                _phantom: PhantomData,
-            }
-        }
+    // TODO; Try removeing `pub`
+    pub trait FromRegex<'a> {
+        fn parse(re: &Regex, s: &'a str) -> Self;
     }
 
-    pub struct LinesToTuples<'a, Tuple, ToTupleType>
-    where
-        ToTupleType: ToTuple<'a, Tuple>,
-    {
-        str_parser: &'a ToTupleType,
-        lines: Lines<'a>,
-        _phantom: PhantomData<Tuple>,
-    }
-
-    impl<'a, Tuple, ToTupleType> Iterator for LinesToTuples<'a, Tuple, ToTupleType>
-    where
-        ToTupleType: ToTuple<'a, Tuple>,
-    {
-        type Item = Tuple;
-
-        fn next(&mut self) -> Option<Tuple> {
-            self.lines.next().map(|s| self.str_parser.to_tuple(s))
-        }
-    }
-
-    impl<'a, T1, T2> ToTuple<'a, (T1, T2)> for Regex
+    impl<'a, T1, T2> FromRegex<'a> for (T1, T2)
     where
         T1: FromStr<'a>,
         T2: FromStr<'a>,
     {
-        fn to_tuple(&self, s: &'a str) -> (T1, T2) {
-            let captures = self.captures(s).unwrap();
+        fn parse(re: &Regex, s: &'a str) -> Self {
+            let captures = re.captures(s).unwrap();
             (
                 T1::from_str(captures.get(1).unwrap().as_str()),
                 T2::from_str(captures.get(2).unwrap().as_str()),
@@ -80,14 +54,14 @@ pub mod to_tuple {
         }
     }
 
-    impl<'a, T1, T2, T3> ToTuple<'a, (T1, T2, T3)> for Regex
+    impl<'a, T1, T2, T3> FromRegex<'a> for (T1, T2, T3)
     where
         T1: FromStr<'a>,
         T2: FromStr<'a>,
         T3: FromStr<'a>,
     {
-        fn to_tuple(&self, s: &'a str) -> (T1, T2, T3) {
-            let captures = self.captures(s).unwrap();
+        fn parse(re: &Regex, s: &'a str) -> Self {
+            let captures = re.captures(s).unwrap();
             (
                 T1::from_str(captures.get(1).unwrap().as_str()),
                 T2::from_str(captures.get(2).unwrap().as_str()),
@@ -96,15 +70,15 @@ pub mod to_tuple {
         }
     }
 
-    impl<'a, T1, T2, T3, T4> ToTuple<'a, (T1, T2, T3, T4)> for Regex
+    impl<'a, T1, T2, T3, T4> FromRegex<'a> for (T1, T2, T3, T4)
     where
         T1: FromStr<'a>,
         T2: FromStr<'a>,
         T3: FromStr<'a>,
         T4: FromStr<'a>,
     {
-        fn to_tuple(&self, s: &'a str) -> (T1, T2, T3, T4) {
-            let captures = self.captures(s).unwrap();
+        fn parse(re: &Regex, s: &'a str) -> Self {
+            let captures = re.captures(s).unwrap();
             (
                 T1::from_str(captures.get(1).unwrap().as_str()),
                 T2::from_str(captures.get(2).unwrap().as_str()),
@@ -113,34 +87,69 @@ pub mod to_tuple {
             )
         }
     }
+
+    // pub trait ToTuple<'a, Tuple>
+    // where
+    //     Self: Sized,
+    // {
+    //     fn parse_regex(&self, s: &'a str) -> Tuple;
+
+    //     fn parse_lines(&'a self, lines: &'a str) -> LinesToTuples<'a, Tuple, Self> {
+    //         LinesToTuples {
+    //             str_parser: self,
+    //             lines: lines.trim().lines(),
+    //             _phantom: PhantomData,
+    //         }
+    //     }
+    // }
+
+    // pub struct LinesToTuples<'a, Tuple, ToTupleType>
+    // where
+    //     ToTupleType: ToTuple<'a, Tuple>,
+    // {
+    //     str_parser: &'a ToTupleType,
+    //     lines: Lines<'a>,
+    //     _phantom: PhantomData<Tuple>,
+    // }
+
+    // impl<'a, Tuple, ToTupleType> Iterator for LinesToTuples<'a, Tuple, ToTupleType>
+    // where
+    //     ToTupleType: ToTuple<'a, Tuple>,
+    // {
+    //     type Item = Tuple;
+
+    //     fn next(&mut self) -> Option<Tuple> {
+    //         self.lines.next().map(|s| self.str_parser.to_tuple(s))
+    //     }
+    // }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::to_tuple::ToTuple;
+    use super::parse_regex::from_regex;
     use regex::Regex;
 
     #[test]
-    fn pair_to_tuple() {
+    fn pair_parse_regex() {
         let re = Regex::new(r"(\d+) -> ([a-z]+)").unwrap();
-        let (a, b): (usize, &str) = re.to_tuple("1 -> abc");
+        let (a, b): (usize, &str) = from_regex(&re, "1 -> abc");
         assert_eq!(a, 1);
         assert_eq!(b, "abc");
     }
 
     #[test]
-    fn triple_to_tuple() {
+    fn triple_parse_regex() {
         let re = Regex::new(r"(\d+) \+\+ ([a-z]+) \+\+ ([a-z])").unwrap();
-        let (a, b, c): (usize, &str, char) = re.to_tuple("99 ++ xyz ++ q");
+        let (a, b, c): (usize, &str, char) = from_regex(&re, "99 ++ xyz ++ q");
         assert_eq!(a, 99);
         assert_eq!(b, "xyz");
         assert_eq!(c, 'q');
     }
 
     #[test]
-    fn quadruple_to_tuple() {
+    fn quadruple_parse_regex() {
         let re = Regex::new(r"(\-?\d+),(\d+),(\d+),(\d+)").unwrap();
-        let (a, b, c, d): (isize, usize, usize, usize) = re.to_tuple("-1,0,1,2");
+        let (a, b, c, d): (isize, usize, usize, usize) = from_regex(&re, "-1,0,1,2");
         assert_eq!(a, -1);
         assert_eq!(b, 0);
         assert_eq!(c, 1);
@@ -148,19 +157,20 @@ mod tests {
     }
 
     #[test]
-    fn lines_to_tuples() {
-        let input = "
-        a => x 
-        b => y 
-        c => z 
-        ";
+    fn lines_parse_regexs() {
+        todo!("Do the line iterator.")
+        // let input  "
+        // a => x 
+        // b => y 
+        // c => z 
+        // ";
 
-        let re = Regex::new(r"([a-z]) => ([a-z])").unwrap();
-        let mut iter = re.parse_lines(input);
+        // let re = Regex::new(r"([a-z]) => ([a-z])").unwrap();
+        // let mut iter = re.parse_lines(input);
 
-        assert_eq!(Some(('a', 'x')), iter.next());
-        assert_eq!(Some(('b', 'y')), iter.next());
-        assert_eq!(Some(('c', 'z')), iter.next());
-        assert_eq!(None, iter.next());
+        // assert_eq!(Some(('a', 'x')), iter.next());
+        // assert_eq!(Some(('b', 'y')), iter.next());
+        // assert_eq!(Some(('c', 'z')), iter.next());
+        // assert_eq!(None, iter.next());
     }
 }
