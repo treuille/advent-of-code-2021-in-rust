8901861fa7c8821bfdaae9e1fdb5367f82732789
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-05-09 18:39:28 -0700

    Finished polishing day 17

--- src/bin/day_17.rs
+++ src/bin/day_17.rs
@@ -1,42 +1,37 @@
-use std::ops::{Range, RangeInclusive};
-
-const TARGET_XS: RangeInclusive<isize> = 269..=292;
-const TARGET_YS: RangeInclusive<isize> = -68..=-44;
+use itertools::iproduct;
 
 fn main() {
-    let min_dx = ((*TARGET_XS.start() as f64) * 2.0).sqrt() as isize;
-    let max_dx = *TARGET_XS.end();
-    let min_dy = *TARGET_YS.start();
-    let max_dy = -*TARGET_YS.start() + 1;
+    let (goal_x_min, goal_x_max): (isize, isize) = (269, 292);
+    let (goal_y_min, goal_y_max): (isize, isize) = (-68, -44);
+
+    let launch_vels_x = (((goal_x_min * 2) as f64).sqrt() as isize)..=goal_x_max;
+    let launch_vels_y = goal_y_min..=(-goal_y_min - 1);
 
     let mut max_height: isize = -1;
     let mut n_hits: usize = 0;
 
-    for launch_dx in min_dx..=max_dx {
-        for launch_dy in min_dy..=max_dy {
-            let mut x: isize = 0;
-            let mut y: isize = 0;
-            let mut dx: isize = launch_dx;
-            let mut dy: isize = launch_dy;
-            let mut max_y: isize = 0;
-            loop {
-                x += dx;
-                y += dy;
-                dx = isize::max(0, dx - 1);
-                dy -= 1;
-                max_y = isize::max(y, max_y);
-                if TARGET_XS.contains(&x) && TARGET_YS.contains(&y) {
-                    max_height = isize::max(max_height, max_y);
-                    n_hits += 1;
-                    break;
-                } else if x > *TARGET_XS.end() || y < *TARGET_YS.start() {
-                    break;
-                }
+    for launch_vel in iproduct!(launch_vels_x, launch_vels_y) {
+        // Launch a new problem with velocity `launch_vel`.
+        let (mut x, mut y): (isize, isize) = (0, 0);
+        let (mut dx, mut dy): (isize, isize) = launch_vel;
+        let mut max_y: isize = 0;
+
+        while x <= goal_x_max && y >= goal_y_min {
+            if x >= goal_x_min && y <= goal_y_max {
+                // We hit the goal. Record this result.
+                max_height = isize::max(max_height, max_y);
+                n_hits += 1;
+                break;
             }
+            // We haven't hit the goal yet, step the simulation.
+            x += dx;
+            y += dy;
+            dx = isize::max(0, dx - 1);
+            dy -= 1;
+            max_y = isize::max(y, max_y);
         }
     }
-    println!("the max_height is {max_height}");
-    println!("there were {n_hits} hits");
-    // println!("17a: {} (123)", solve_17a());;
-    // println!("17b: {} (456)", solve_17b());
+
+    println!("17a: {} (2278)", max_height);
+    println!("17b: {} (996)", n_hits);
 }
