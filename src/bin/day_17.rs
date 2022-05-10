de556da898984f0bc555b91bb8448847fdd2fcd5
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-05-09 18:43:22 -0700

    A tiny bit more polishing

--- src/bin/day_17.rs
+++ src/bin/day_17.rs
@@ -1,13 +1,15 @@
 use itertools::iproduct;
 
 fn main() {
+    // The target goal.
     let (goal_x_min, goal_x_max): (isize, isize) = (269, 292);
     let (goal_y_min, goal_y_max): (isize, isize) = (-68, -44);
 
+    // The range of potentially viable start velocities. 
     let launch_vels_x = (((goal_x_min * 2) as f64).sqrt() as isize)..=goal_x_max;
-    let launch_vels_y = goal_y_min..=(-goal_y_min - 1);
+    let launch_vels_y = goal_y_min..-goal_y_min;
 
-    let mut max_height: isize = -1;
+    let mut max_height: isize = isize::MIN;
     let mut n_hits: usize = 0;
 
     for launch_vel in iproduct!(launch_vels_x, launch_vels_y) {
@@ -23,6 +25,7 @@ fn main() {
                 n_hits += 1;
                 break;
             }
+
             // We haven't hit the goal yet, step the simulation.
             x += dx;
             y += dy;
