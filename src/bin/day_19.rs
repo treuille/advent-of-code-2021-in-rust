75d1e34bf4b80a28f79cceda1f613b90d289a93b
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-05-16 10:54:19 -0700

    Starting to work on Beacons

--- src/bin/day_19.rs
+++ src/bin/day_19.rs
@@ -1,11 +1,33 @@
 use aoc::parse_regex::parse_lines;
+use itertools::iproduct;
 use ndarray::prelude::*;
 use regex::Regex;
 
 fn main() {
-    for (i, beacon) in read_input().into_iter().enumerate() {
-        println!("beacon {i}:\n{beacon:?}");
+    let mut beacons = read_input();
+    let mut unsolved = beacons.split_off(1);
+    let mut solved = beacons;
+
+    while !unsolved.is_empty() {
+        // let mut unsolved2 = Vec::with_capacity(unsolved.len() - 1);
+        // let candidates = unsolved.drain();
+        todo!("Move beacons unsolved -> solved");
+        // while let Some(beacon2) = candidates.next() {
+        // }
+        // }
+        // for (beacon1, beacon2) in iproduct!(solved.iter(), .enumerate()) {
+        //     match align(beacon1, beacon2) {
+        //         Ok(beacon2) => {
+        //             solved.push(beacon2);
+        //             break;
+        //         },
+        //         Err(beacon2)
+        //     }
+        //     tood!("What happens in this loop?")
+        // }
     }
+    println!("solved: {}", solved.len());
+    println!("unsolved: {}", unsolved.len());
 }
 
 type Beacon = Vec<Pt>;
@@ -19,6 +41,11 @@ type Pt = Array1<i64>;
 // fn solve_XXb() -> usize {
 //     456
 // }
+
+/// Ok(beacon2) if they can be aligned, Err(beacon2) otherwise.
+fn align(beacon1: &Beacon, beacon2: Beacon) -> Result<Beacon, Beacon> {
+    todo!("align")
+}
 
 fn parse_beacon(s: &str) -> Beacon {
     let s = s.split_once("\n").unwrap().1;
