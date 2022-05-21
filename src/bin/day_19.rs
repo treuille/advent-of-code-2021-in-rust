68d782508eeb3953a1289e5e31863db40c900717
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-05-21 00:44:13 -0700

    It seems that align() is now working

--- src/bin/day_19.rs
+++ src/bin/day_19.rs
@@ -1,8 +1,10 @@
 use aoc::parse_regex::parse_lines;
-// use itertools::iproduct;
+use itertools::iproduct;
+use std::collections::{HashMap, HashSet};
 // use ndarray::prelude::*;
 use regex::Regex;
 use std::fmt::{Debug, Error as FormatErr, Formatter};
+use std::ops::{Add, Sub};
 
 fn main() {
     println!("rotations: {:?}", ROTATIONS);
@@ -15,6 +17,18 @@ fn main() {
     let scanner2 = scanners.next().unwrap();
     let scanner3 = align(&scanner1, scanner2);
     println!("scanner3: {:?}", scanner3);
+    if let Ok(scanner3) = scanner3 {
+        let scanner1: HashSet<Beacon> = scanner1.0.into_iter().collect();
+        let mut n_matches: usize = 0;
+        for beacon3 in scanner3.0.iter() {
+            if scanner1.contains(beacon3) {
+                n_matches += 1;
+                println!("{beacon3:?} -> {n_matches}");
+            } else {
+                println!("{beacon3:?} XXX");
+            }
+        }
+    }
 }
 
 // fn search_for_alignment(mut scanners: Vec<Scanner>) {
@@ -79,27 +93,80 @@ fn main() {
 
 /// Ok(scanner2) if they can be aligned, Err(scanner2) otherwise.
 fn align(scanner1: &Scanner, scanner2: Scanner) -> Result<Scanner, Scanner> {
-    // for rot in ROTATIONS.iter() {
-    //     println!("testing rotation: {:?}", rot);
-    //     let scanner2_rot: Scanner = scanner2.iter().map(|beacon| rotate(beacon, rot)).collect();
-    //     let translations: HashMap<Translation, usize) = HashMap::new();
-    //     // for (beacon1, beacon2) in iproduct!(scanner1.iter(), scanner2.iter()) {
+    for rot in ROTATIONS.iter() {
+        println!("testing rotation: {:?}", rot);
 
-    //     // }
-    // }
-    todo!("Must finish align()")
+        // TODO: rename to scanner2
+        let scanner2_rot: Scanner = scanner2.rotate(rot);
+
+        let mut translations: HashMap<Translation, usize> = HashMap::new();
+        for (beacon1, beacon2) in iproduct!(scanner1.0.iter(), scanner2_rot.0.iter()) {
+            let translation: Translation = beacon1 - beacon2;
+            *translations.entry(translation).or_default() += 1;
+        }
+        println!("found {} translations", translations.len());
+        println!("max entries: {:?}", translations.values().max());
+        println!("min entries: {:?}", translations.values().min());
+        println!();
+
+        let successful_translations: Vec<Translation> = translations
+            .into_iter()
+            .filter_map(|(translation, n_translations)| (n_translations >= 12).then(|| translation))
+            .collect();
+
+        let translation: &Translation = match successful_translations.len() {
+            0 => continue,
+            1 => successful_translations.get(0).unwrap(),
+            n => panic!("Too many successful translations for {rot:?}: {n}"),
+        };
+        return Ok(Scanner(
+            scanner2_rot
+                .0
+                .iter()
+                .map(|beacon| beacon + translation)
+                .collect(),
+        ));
+    }
+    Err(scanner2)
 }
 
-// A set of
+/// A becon emitting informaton.
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Beacon(i64, i64, i64);
+
+/// Adding a Translation to a Beacon yields another Beacon
+impl Add<&Translation> for &Beacon {
+    type Output = Beacon;
+
+    fn add(self, rhs: &Translation) -> Self::Output {
+        Beacon(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    }
+}
+
+/// Subtraction two Beacon gives a Translation.
+impl Sub for &Beacon {
+    type Output = Translation;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Translation(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
+    }
+}
+
+/// A set of detected beacons.
 #[derive(Debug)]
 struct Scanner(Vec<Beacon>);
 
-#[derive(Debug)]
-struct Beacon(i64, i64, i64);
+impl Scanner {
+    /// Rotate all the beacons in a scanner.
+    fn rotate(&self, rot: &Rotation) -> Scanner {
+        Scanner(self.0.iter().map(|beacon| rot.apply(beacon)).collect())
+    }
+}
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct Translation(i64, i64, i64);
 
+#[derive(Clone, Copy)]
 enum SignedAxis {
     NegX,
     NegY,
@@ -124,6 +191,21 @@ impl Debug for SignedAxis {
 
 // 1=i NegX=-i 2=j NegY=-j 3=k NegZ=-k
 struct Rotation(SignedAxis, SignedAxis, SignedAxis);
+
+// Rotates the point along the origin by the given amount
+impl Rotation {
+    fn apply(&self, beacon: &Beacon) -> Beacon {
+        let coord = |axis: SignedAxis| match axis {
+            SignedAxis::NegX => -beacon.0,
+            SignedAxis::NegY => -beacon.1,
+            SignedAxis::NegZ => -beacon.2,
+            SignedAxis::PosX => beacon.0,
+            SignedAxis::PosY => beacon.1,
+            SignedAxis::PosZ => beacon.2,
+        };
+        Beacon(coord(self.0), coord(self.1), coord(self.2))
+    }
+}
 
 impl Debug for Rotation {
     fn fmt<'a>(&self, formatter: &mut Formatter<'a>) -> Result<(), FormatErr> {
@@ -161,20 +243,6 @@ const ROTATIONS: [Rotation; 24] = [
     Rotation(SignedAxis::PosZ, SignedAxis::PosY, SignedAxis::NegX),
     Rotation(SignedAxis::NegZ, SignedAxis::NegY, SignedAxis::NegX),
 ];
-
-// /// Rotates the point along the origin by the given amount
-// fn rotate(pt: &Beacon, rot: &Rotation) -> Beacon {
-//     let coord = |axis: i8| match axis {
-//         1 => pt.0,
-//         2 => pt.1,
-//         3 => pt.2,
-//         NegX => -pt.0,
-//         NegY => -pt.1,
-//         NegZ => -pt.2,
-//         _ => unimplemented!("Axis must be a nonzero integer between NegZ and 3"),
-//     };
-//     (coord(rot.0), coord(rot.1), coord(rot.2))
-// }
 
 fn parse_beacon(s: &str) -> Scanner {
     let (_, s) = s.split_once("\n").unwrap();
