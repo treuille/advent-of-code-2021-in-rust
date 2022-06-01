2477e464902ecbf86f0cf75a692ec1d298cd1917
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-05-31 20:46:10 -0700

    Parsed the first string

--- src/bin/day_20.rs
+++ src/bin/day_20.rs
@@ -17,7 +17,8 @@ const DEMO_INPUT: &str = "
 ";
 
 fn main() {
-    println!("result: {}", read_input(DEMO_INPUT));
+    let (lookup_table, image) = read_input(DEMO_INPUT.trim());
+    println!("result: {}", false);
 
     // for (i, line) in read_input().into_iter().enumerate() {
     //     println!("{i}: \"{line}\"");
@@ -34,13 +35,31 @@ fn main() {
 //     456
 // }
 
-// type Pt = (i64, i64);
-// type Image = HashSet<Pt>;
-// type LookupTable = [bool; 512];
+type Pt = (i64, i64);
+type Image = HashSet<Pt>;
+type LookupTable = [bool; 512];
 
 /// Read the input file and turn it into an Array2<u8>
-fn read_input(_input: &str) -> bool {
-    false
+fn read_input(input: &str) -> (LookupTable, Image) {
+    let (lookup_table_str, image_str) = input.split_once("\n\n").unwrap();
+    println!("lookup_table:\n{lookup_table_str}");
+    println!("image:\n{image_str}");
+    // println!(
+    //     "lookup_table: {:?}",
+    //     lookup_table.chars().collect::<Vec<_>>()
+    // );
+    let lookup_table = [false; 512];
+    lookup_table_str
+        .chars()
+        .filter_map(|c| match c {
+            '.' => Some(false),
+            '#' => Some(true),
+            _ => None,
+        })
+        .enumerate()
+        .filter_map(|(i, b)| b.then(|| i))
+        .for_each(|i| println!("{i}"));
+    todo!("Implement read_input");
     // include_str!("../../puzzle_inputs/day_XX.txt")
     //     .lines()
     //     .collect()
