12f12f40be290a15a027a8977882016d9b6a9d3e
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-06-01 10:41:46 -0700

    Grid working

--- src/bin/day_20.rs
+++ src/bin/day_20.rs
@@ -1,6 +1,8 @@
-use itertools::{iproduct, Itertools};
-use std::borrow::Borrow;
-use std::collections::HashSet;
+// use itertools::{iproduct, Itertools};
+use ndarray::prelude::*;
+use ndarray::Zip;
+// use std::borrow::Borrow;
+// use std::collections::HashSet;
 
 #[allow(dead_code)]
 const DEMO_INPUT: &str = "
@@ -20,21 +22,30 @@ const DEMO_INPUT: &str = "
 ";
 
 fn main() {
-    let input = include_str!("../../puzzle_inputs/day_20.txt").trim();
-    // let input = DEMO_INPUT.trim();
+    // let input = include_str!("../../puzzle_inputs/day_20.txt").trim();
+    let input = DEMO_INPUT.trim();
     let (lookup_table, image) = read_input(input);
-    print_image(&image);
+    print_image(&image.view());
     println!();
 
-    let image = enhance(&image, &lookup_table);
-    print_image(&image);
+    let runs = 2;
+    let mut image = grow(image, runs);
+    print_image(&image.view());
     println!();
 
-    let image = enhance(&image, &lookup_table);
-    print_image(&image);
-    println!();
+    for i in 0..runs {
+        image = enhance(image, &lookup_table);
+        println!("iteration: {i}");
+        print_image(&image.view());
+        println!();
+    }
+    // let image = enhance(&image, &lookup_table);
 
-    println!("len: {}", image.len());
+    // let image = enhance(&image, &lookup_table);
+    // print_image(&image);
+    // println!();
+
+    // println!("len: {}", image.len());
 }
 
 // fn solve_XXa() -> usize {
@@ -45,68 +56,77 @@ fn main() {
 //     456
 // }
 
-type Pt = (i64, i64);
-type Image = HashSet<Pt>;
+// type Pt = (i64, i64);
+// type Pt = (usize, usize);
+type Image = Array2<bool>;
 type LookupTable = [bool; 512];
 
-fn enhance(image: &Image, lookup_table: &LookupTable) -> Image {
-    image
-        .iter()
-        .flat_map(neighbors)
-        .unique()
-        .flat_map(|pt1| {
-            let indx =
-                neighbors(pt1)
-                    .enumerate()
-                    .fold(0, |indx, (i, pt2)| match image.contains(&pt2) {
-                        false => indx,
-                        true => indx | 1 << (8 - i),
-                    });
-            // println!("{pt1:?} -> {indx}");
-            lookup_table[indx].then(|| pt1)
-        })
-        .collect()
+fn grow(image: Image, runs: usize) -> Image {
+    let (w, h) = image.dim();
+    let mut bigger_image = Array2::from_elem((w + 4 * runs, h + 4 * runs), false);
+    let slice = s![(2 * runs)..(2 * runs + w), (2 * runs)..(2 * runs + h)];
+    bigger_image.slice_mut(slice).assign(&image);
+    bigger_image
 }
 
-fn neighbors(pt: impl Borrow<Pt>) -> impl Iterator<Item = Pt> {
-    let (i, j) = pt.borrow();
-    iproduct!((j - 1)..=(j + 1), (i - 1)..=(i + 1)).map(|(j, i)| (i, j))
+fn enhance(image: Image, lookup_table: &LookupTable) -> Image {
+    // image.windows((3, 3)).for_each(|window| {
+    //     println!("{:?}", window);
+    // });
+    Zip::from(image.windows((3, 3))).map_collect(|window| {
+        let indx = window.iter().enumerate().fold(0, |indx, (i, x)| match x {
+            false => indx,
+            true => indx | 1 << (8 - i),
+        });
+        lookup_table[indx]
+    })
+    // todo!("ehance {:?}", x.dim());
+    // image
+    //     .iter()
+    //     .flat_map(neighbors)
+    //     .unique()
+    // , lookup_table: &LookupTable   //     .flat_map(|pt1| {
+    //         let indx =
+    //             neighbors(pt1)
+    //                 .enumerate()
+    //                 .fold(0, |indx, (i, pt2)| match image.contains(&pt2) {
+    //                     false => indx,
+    //                     true => indx | 1 << (8 - i),
+    //                 });
+    //         // println!("{pt1:?} -> {indx}");
+    //         lookup_table[indx].then(|| pt1)
+    //     })
+    //     .collect()
 }
 
-fn print_image(image: &Image) {
-    let min_i = *image.iter().map(|(i, _)| i).min().unwrap();
-    let max_i = *image.iter().map(|(i, _)| i).max().unwrap();
-    let min_j = *image.iter().map(|(_, j)| j).min().unwrap();
-    let max_j = *image.iter().map(|(_, j)| j).max().unwrap();
-    for j in min_j..=max_j {
-        for i in min_i..=max_i {
-            match image.contains(&(i, j)) {
-                true => print!("# "),
-                false => print!(". "),
-            }
-        }
-        println!();
+// fn neighbors(pt: impl Borrow<Pt>) -> impl Iterator<Item = Pt> {
+//     let (i, j) = pt.borrow();
+//     iproduct!((j - 1)..=(j + 1), (i - 1)..=(i + 1)).map(|(j, i)| (i, j))
+// }
+
+fn print_image(image: &ArrayView2<bool>) {
+    for row in image.rows() {
+        let row_chars = row.iter().map(|x| match x {
+            false => '.',
+            true => '#',
+        });
+        println!("{}", row_chars.collect::<String>());
     }
-    println!("({min_i}->{max_i})x({min_j}->{max_j}) : {}", image.len());
+    println!("{:?}", image.dim());
 }
 
-fn parse_image(image_str: &str) -> Image {
-    image_str
-        .lines()
-        .enumerate()
-        .flat_map(|(j, line)| {
-            line.chars()
-                .enumerate()
-                .filter_map(move |(i, c)| (c == '#').then(|| (i as i64, j as i64)))
-        })
-        .collect()
+fn parse_grid(input: &str) -> Image {
+    let mut lines = input.lines().peekable();
+    let w = lines.peek().unwrap().len();
+    let array: Array1<bool> =
+        Array::from_iter(lines.flat_map(|line| line.chars().map(|c| c == '#')));
+    let h = array.len() / w;
+    array.into_shape((w, h)).unwrap()
 }
 
 /// Read the input file and turn it into an Array2<u8>
 fn read_input(input: &str) -> (LookupTable, Image) {
     let (lookup_table_str, image_str) = input.split_once("\n\n").unwrap();
-    println!("lookup_table:\n{lookup_table_str}");
-    println!("image:\n{image_str}");
 
     // Parse the lookup table
     let mut lookup_table = [false; 512];
@@ -120,7 +140,6 @@ fn read_input(input: &str) -> (LookupTable, Image) {
         .enumerate()
         .filter_map(|(i, b)| b.then(|| i))
         .for_each(|i| lookup_table[i] = true);
-    // panic!("lookup_table_str.len(): {}", lookup_table_str.len());
 
-    (lookup_table, parse_image(image_str))
+    (lookup_table, parse_grid(image_str))
 }
