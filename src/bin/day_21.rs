0ad111b32485765eb25f7e1de5aa838468f6892d
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-06-02 05:14:03 -0700

    Fully cleaned up 21 a and b

--- src/bin/day_21.rs
+++ src/bin/day_21.rs
@@ -1,8 +1,7 @@
-use std::collections::HashMap;
+use cached::proc_macro::cached;
 
 fn main() {
     let state = State::new(1, 5);
-    // let state = State::new(4, 8);
 
     println!("21a: {} (432450)", solve_21a(state.clone()));
     println!("21b: {} (138508043837521)", solve_21b(state.clone()));
@@ -30,40 +29,23 @@ fn solve_21a(mut state: State) -> usize {
 }
 
 fn solve_21b(state: State) -> usize {
-    let mut wins_table: DiracWins = DiracWins::new();
-    let [w1, w2] = wins_dirac(&state, &mut wins_table);
+    let [w1, w2] = wins_dirac(state);
     usize::max(w1, w2)
 }
 
-/// From this starting position, in how many universes does each player win?
-fn wins_dirac(state: &State, wins_table: &mut DiracWins) -> [usize; 2] {
+#[cached]
+fn wins_dirac(state: State) -> [usize; 2] {
     match state.score {
-        [score_1, _] if score_1 >= 21 => {
-            assert_eq!(state.player, 1);
-            [1, 0]
-        }
-        [_, score_2] if score_2 >= 21 => {
-            assert_eq!(state.player, 0);
-            [0, 1]
-        }
-        _ => {
-            #[allow(clippy::map_entry)]
-            if !wins_table.contains_key(state) {
-                let answer =
-                    state
-                        .step_dirac()
-                        .fold([0, 0], |[wins_1, wins_2], (child, frequency)| {
-                            let [child_w1, child_w2] = wins_dirac(&child, wins_table);
-                            [wins_1 + child_w1 * frequency, wins_2 + child_w2 * frequency]
-                        });
-                wins_table.insert(state.clone(), answer);
-            }
-            wins_table[state]
-        }
+        [score_1, _] if score_1 >= 21 => [1, 0],
+        [_, score_2] if score_2 >= 21 => [0, 1],
+        _ => state
+            .step_dirac()
+            .fold([0, 0], |[wins_1, wins_2], (child, frequency)| {
+                let [child_w1, child_w2] = wins_dirac(child);
+                [wins_1 + child_w1 * frequency, wins_2 + child_w2 * frequency]
+            }),
     }
 }
-
-type DiracWins = HashMap<State, [usize; 2]>;
 
 #[derive(Clone, Hash, PartialEq, Eq)]
 struct State {
