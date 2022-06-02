8b15fee94a794a2746bdf65499a8ada7c964bcf4
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-06-01 21:24:34 -0700

    Solved 21a

--- src/bin/day_21.rs
+++ src/bin/day_21.rs
@@ -1,13 +1,27 @@
 use std::iter;
 
 fn main() {
-    // println!("XXa: {} (123)", solve_XXa());
-    // println!("XXb: {} (456)", solve_XXb());
-    // let mut pos = [3, 7];
+    // let mut pos = [0, 4];
+    let mut pos = [3, 7];
+    let mut score = [0, 0];
+    let mut player = 0;
+    let mut rolls = 0;
     let mut die = get_die();
-    for i in 0..10 {
-        println!("{i} -> {:?}", die.next());
+
+    while score.iter().max().unwrap() < &1000 {
+        let roll: usize = [die.next(), die.next(), die.next()]
+            .into_iter()
+            .flatten()
+            .sum();
+        pos[player] = (pos[player] + roll) % 10;
+        score[player] += pos[player] + 1;
+        player = (player + 1) % 2;
+        rolls += 3;
     }
+    let loser_score = score.iter().min().unwrap();
+    println!("loser_score: {loser_score}");
+    println!("rolls: {rolls}");
+    println!("answer: {}", loser_score * rolls);
 }
 
 fn get_die() -> impl Iterator<Item = usize> {
@@ -17,11 +31,3 @@ fn get_die() -> impl Iterator<Item = usize> {
         state + 1
     })
 }
-
-// fn solve_XXa() -> usize {
-//     123
-// }
-
-// fn solve_XXb() -> usize {
-//     456
-// }
