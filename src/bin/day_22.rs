709ac67b4bfad530f22bf7acafa87a8b178956a9
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-06-06 17:47:51 -0700

    Starting to clean things up (324 loc)

--- src/bin/day_22.rs
+++ src/bin/day_22.rs
@@ -8,75 +8,16 @@ use std::iter::Iterator;
 use std::ops::Range;
 use std::rc::Rc;
 
-const TEST_INPUT_1: &str = "
-on x=10..12,y=10..12,z=10..12
-on x=11..13,y=11..13,z=11..13
-off x=9..11,y=9..11,z=9..11
-on x=10..10,y=10..10,z=10..10";
-
-const TEST_INPUT_2: &str = "
-on x=-20..26,y=-36..17,z=-47..7
-on x=-20..33,y=-21..23,z=-26..28
-on x=-22..28,y=-29..23,z=-38..16
-on x=-46..7,y=-6..46,z=-50..-1
-on x=-49..1,y=-3..46,z=-24..28
-on x=2..47,y=-22..22,z=-23..27
-on x=-27..23,y=-28..26,z=-21..29
-on x=-39..5,y=-6..47,z=-3..44
-on x=-30..21,y=-8..43,z=-13..34
-on x=-22..26,y=-27..20,z=-29..19
-off x=-48..-32,y=26..41,z=-47..-37
-on x=-12..35,y=6..50,z=-50..-2
-off x=-48..-32,y=-32..-16,z=-15..-5
-on x=-18..26,y=-33..15,z=-7..46
-off x=-40..-22,y=-38..-28,z=23..41
-on x=-16..35,y=-41..10,z=-47..6
-off x=-32..-23,y=11..30,z=-14..3
-on x=-49..-5,y=-3..45,z=-29..18
-off x=18..30,y=-20..-8,z=-3..13
-on x=-41..9,y=-7..43,z=-33..15
-on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
-on x=967..23432,y=45373..81175,z=27513..53682";
-
-const MY_INPUT_1: &str = "
-on x=-20..26,y=-36..17,z=-47..7
-on x=-20..33,y=-21..23,z=-26..28
-on x=-22..28,y=-29..23,z=-38..16
-on x=-46..7,y=-6..46,z=-50..-1
-on x=-49..1,y=-3..46,z=-24..28
-on x=2..47,y=-22..22,z=-23..27
-on x=-27..23,y=-28..26,z=-21..29
-on x=-39..5,y=-6..47,z=-3..44
-on x=-30..21,y=-8..43,z=-13..34
-on x=-22..26,y=-27..20,z=-29..19
-off x=-48..-32,y=26..41,z=-47..-37
-on x=-12..35,y=6..50,z=-50..-2
-off x=-48..-32,y=-32..-16,z=-15..-5
-on x=-18..26,y=-33..15,z=-7..46
-off x=-40..-22,y=-38..-28,z=23..41
-on x=-16..35,y=-41..10,z=-47..6
-off x=-32..-23,y=11..30,z=-14..3
-on x=-49..-5,y=-3..45,z=-29..18
-off x=18..30,y=-20..-8,z=-3..13
-on x=-41..9,y=-7..43,z=-33..15";
-
-// b todo:
-// - keep a mutable list of cubes
-// - update that list whenever we need to
-
 fn main() {
-    // let input = TEST_INPUT_2;
-    // let input = TEST_INPUT_1;
-    // let input = MY_INPUT_1;
     let input = include_str!("../../puzzle_inputs/day_22.txt");
     let steps = parse_input(input);
-    println!("22a: {}", solve_22a(&steps));
+    println!("22a: {} (652209)", solve_22a(&steps));
     println!("22b: {:?} (1217808640648260)", solve_22b(steps));
 }
 
 fn solve_22a(steps: &[Step]) -> usize {
     let bound = Cube([-50..51, -50..51, -50..51]);
-    let steps: Vec<Step> = steps.iter().filter_map(|step| step.clamp(&bound)).collect();
+    let steps = Step::clamp(steps, &bound); // Vec<Step> = steps.iter().filter_map(|step| step.clamp(&bound)).collect();
     let mut grid: HashSet<Pt> = HashSet::new();
     let ignore = |_: bool| ();
     for step in steps {
@@ -108,14 +49,16 @@ fn solve_recursively(steps: Vec<Step>) -> isize {
     sub_bounds
         .iter()
         .map(|sub_bound| {
-            let steps: Vec<Step> = steps
-                .iter()
-                .filter_map(|step| step.clamp(sub_bound))
-                .collect();
-            assert!(
-                steps.iter().all(|step| sub_bound.contains(&step.cube)),
-                "Bounds constraint doesn't hold."
-            );
+            let steps = Step::clamp(&steps, sub_bound);
+            // Vec<Step> = steps.iter().filter_map(|step| step.clamp(&bound)).collect();
+            // let steps: Vec<Step> = steps
+            //     .iter()
+            //     .filter_map(|step| step.clamp(sub_bound))
+            //     .collect();
+            // assert!(
+            //     steps.iter().all(|step| sub_bound.contains(&step.cube)),
+            //     "Bounds constraint doesn't hold."
+            // );
             // println!(
             //     "-> recursing into {:?} with {} steps",
             //     sub_bound,
@@ -321,13 +264,17 @@ impl Step {
         }
     }
 
-    fn clamp(&self, bound: &Cube) -> Option<Self> {
-        let cube = bound.clamp(&self.cube)?;
-        assert!(bound.contains(&cube), "{:?} must contain {:?}", bound, cube);
-        Some(Self {
-            additive: self.additive,
-            cube,
-        })
+    /// Clamp the steps to within the bounds, discarding those lying outside.
+    fn clamp(steps: &[Self], bound: &Cube) -> Vec<Self> {
+        steps
+            .iter()
+            .filter_map(|step| {
+                bound.clamp(&step.cube).map(|clamped_cube| Self {
+                    additive: step.additive,
+                    cube: clamped_cube,
+                })
+            })
+            .collect()
     }
 }
 
