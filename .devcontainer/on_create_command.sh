c3e17c8a2a314dc7cca64986a7dd3241d2e20d5f
Adrien Treuille <treuille@users.noreply.github.com>
2022-11-20 22:13:39 +0000

    Now installing tmux

--- .devcontainer/on_create_command.sh
+++ .devcontainer/on_create_command.sh
@@ -17,6 +17,9 @@ cargo install lsd
 # Install ripgrep 
 sudo apt install ripgrep -y
 
+# Install tmux
+sudo apt install tmux -y
+
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
