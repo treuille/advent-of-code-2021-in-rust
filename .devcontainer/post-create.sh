725353d1f56d499d7443aa3f7ff002049ed851c3
Adrien Treuille <treuille@users.noreply.github.com>
2022-11-20 02:27:13 +0000

    Trying to use post-create.sh instead of Dockerfile

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Install the version of Bundler.
+echo "/home/codespace/lil_test_2.txt"
+# touch /home/codespace/lil_test_2.txt
+
+# if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
+#     cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+# fi
+# 
+# # If there's a Gemfile, then run `bundle install`
+# # It's assumed that the Gemfile will install Jekyll too
+# if [ -f Gemfile ]; then
+#     bundle install
+# else
+#     # If there's no Gemfile, install Jekyll
+#     sudo gem install jekyll
+# fi
