fba24da0f7424772df514e66bdaf22404da6ccf1
Adrien Treuille <treuille@users.noreply.github.com>
2022-11-19 21:33:39 -0800

    Cleared out a init command which was running interacitvely.

--- .devcontainer/on_create_command.sh
+++ .devcontainer/on_create_command.sh
@@ -3,13 +3,13 @@ whoami > whoami_1.txt
 whoami > /home/codespace/whoami_2.txt    1  nvim 
 
 # Install neovim 
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim -Y
+# sudo add-apt-repository ppa:neovim-ppa/stable
+# sudo apt-get update
+# sudo apt-get install neovim -Y
 
 # Install: https://github.com/junegunn/vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #    13  nvim 
 #    14  cd ~/
